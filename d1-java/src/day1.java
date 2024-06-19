adea5279f7c897fd5855b30f287e0f36bb98ef87
Trang <kaito2372000@gmail.com>
2024-06-19 16:17:05 +0900

    moi

--- d1-java/src/day1.java
+++ d1-java/src/day1.java
@@ -5,6 +5,8 @@ public class day1 {
         // 출력
         System.out.println("Hello World!!!!");
         System.out.println("Im Trang");
+        System.out.println(3 * 5);
+        System.out.println(3 + 7);
 
         // 입력 받고 출력
         Scanner scanner = new Scanner(System.in);
