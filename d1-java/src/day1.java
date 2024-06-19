e4ba78c184dd6cc5dadfb9ac98609dcada7e4a99
Trang <kaito2372000@gmail.com>
2024-06-19 15:55:49 +0900

    java1

---
+++ d1-java/src/day1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class day1 {
+    public static void main(String[] args) {
+        // 출력
+        System.out.println("Hello World!!!!");
+        System.out.println("Im Trang");
+
+        // 입력 받고 출력
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+
+        // 해당 내용 각 주석으로 정이하기
+    }
+}
